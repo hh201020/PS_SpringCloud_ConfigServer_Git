a745fd113902bdbc8c2af275149dd8dd04c006c1
Jerry Hu <hh201020@gmail.com>
2016-11-28 16:28:17 -0500

    First Commit

---
+++ src/main/java/com/pluralsight/PsSpringCloudConfigServerGitApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsSpringCloudConfigServerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsSpringCloudConfigServerGitApplication.class, args);
+	}
+}
